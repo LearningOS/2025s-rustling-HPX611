a07f293d8b1e3e2c3d9d138adbf5271e47f7087d
HPX <3169289310@qq.com>
2025-04-09 06:53:03 -0700

    updata

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,37 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
+
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.is_empty()
+        {
+            return Person::default()
+        }
+        let part : Vec<&str> = s.split(',').collect();
+
+        if part.len() != 2{
+            return Person::default()
+        }
+
+        let name = part.get(0).unwrap_or(&"").to_string();
+        if name.is_empty()
+        {
+            return Person::default()
+        }
+        
+        let age = match part.get(1) {
+            Some(str) => {
+                match str.parse::<usize>(){
+                    Ok(age) => age,
+                    Err(_) =>  return Person::default()
+                }
+            }
+            None => return Person::default(),
+        };
+
+        Person {name ,age}
     }
 }
 
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,7 +31,7 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
+
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -52,6 +52,28 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.is_empty()
+        {
+            return Err(ParsePersonError::Empty)
+        }
+        let part : Vec<&str> = s.split(',').collect();
+
+        if part.len() != 2{
+            return Err(ParsePersonError::BadLen)
+        }
+
+        let name = part.get(0).unwrap_or(&"").to_string();
+        if name.is_empty()
+        {
+            return Err(ParsePersonError::NoName)
+        }
+        
+        let age = match part[1].parse::<usize>() {
+            Ok(age) => age,
+            Err(a) => return Err(ParsePersonError::ParseInt(a)),
+        };
+
+        Ok(Person {name ,age})
     }
 }
 
